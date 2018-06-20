5346b5a97aca609571736687e9102c5506a9fb02
David Brownman <beamneocube@gmail.com>
2018-06-19 23:06:24 -0700

    api key validation not working now

--- src/index.ts
+++ src/index.ts
@@ -1,129 +1,113 @@
-import * as Koa from 'koa'
-import * as router from 'koa-route'
-import * as bodyParser from 'koa-bodyparser'
+// import * as Koa from 'koa'
+// import * as router from 'koa-route'
+// import * as bodyParser from 'koa-bodyparser'
+import * as express from 'express'
+import * as bodyParser from 'body-parser'
+
 import * as _ from 'lodash'
 import * as dotenv from 'dotenv'
 import pickHanlder, { identifiersByDomain } from './handlerPicker'
 import { validateApiKey, validateInput } from './middlewares'
 
-const app = new Koa()
+const app = express()
 
-app.use(bodyParser())
-if (app.env === 'development') {
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+if (app.get('env') === 'development') {
   dotenv.config()
 }
 
 const WebClient = require('@slack/client').WebClient
 const slackClient = new WebClient(process.env.SLACK_API_TOKEN)
 
-// top level error handler
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (e) {
-    ctx.status = e.status || 500
-    ctx.body = {
-      message: e.message
-    }
-  }
+// give json instructions
+app.get('/', async (req, res) => {
+  res.json({
+    routes: [
+      {
+        description:
+          "mapping of domain to available identifiers. If a domain isn't listed (or you use a listed domain without an identifier), the default handler will be used",
+        route: '/identifiers',
+        method: 'GET'
+      },
+      {
+        description: 'array of Slack channel objects',
+        route: '/channels',
+        method: 'GET'
+      },
+      {
+        description: 'mark an item as read',
+        params: { ts: "message's timestamp", channel: 'slack channel id' },
+        route: '/read',
+        method: 'POST'
+      },
+      {
+        description: 'send an item to a channel',
+        params: {
+          channel: 'slack channel id',
+          identifier:
+            '[optional] string to mark specific content in a shared domain',
+          url: '[optional] url to a linked post',
+          re_parse:
+            "[optional, bool] use an extra parser if Slack doesn't unfurl the link"
+        },
+        route: '/item',
+        method: 'POST'
+      }
+    ]
+  })
 })
 
-// give json instructions
-app.use(
-  router.get('/', async ctx => {
-    ctx.body = {
-      routes: [
-        {
-          description:
-            "mapping of domain to available identifiers. If a domain isn't listed (or you use a listed domain without an identifier), the default handler will be used",
-          route: '/identifiers',
-          method: 'GET'
-        },
-        {
-          description: 'array of Slack channel objects',
-          route: '/channels',
-          method: 'GET'
-        },
-        {
-          description: 'mark an item as read',
-          params: { ts: "message's timestamp", channel: 'slack channel id' },
-          route: '/read',
-          method: 'POST'
-        },
-        {
-          description: 'send an item to a channel',
-          params: {
-            channel: 'slack channel id',
-            identifier:
-              '[optional] string to mark specific content in a shared domain',
-            url: '[optional] url to a linked post',
-            re_parse:
-              "[optional, bool] use an extra parser if Slack doesn't unfurl the link"
-          },
-          route: '/item',
-          method: 'POST'
-        }
-      ]
-    }
-  })
-)
-
-app.use(
-  router.get('/identifiers', async ctx => {
-    ctx.body = identifiersByDomain
-  })
-)
+app.get('/identifiers', (req, res) => {
+  res.json(identifiersByDomain)
+})
 
 // list channels
-app.use(
-  router.get('/channels', async (ctx, next) => {
-    await next()
-    validateApiKey(ctx)
+app.get('/channels', async (req, res, next) => {
+  validateApiKey(req.query.api_key, next)
 
-    const channels = (await slackClient.channels.list()).channels.filter(
-      (i: any) => !i.is_archived
-    )
-    ctx.body = channels.map((channel: any) => _.pick(channel, ['id', 'name']))
-  })
-)
+  const channels = (await slackClient.channels.list()).channels.filter(
+    (i: any) => !i.is_archived
+  )
+  res.json(channels.map((channel: any) => _.pick(channel, ['id', 'name'])))
+})
 
 // mark item as read
-app.use(
-  router.post('/read', async (ctx, next) => {
-    await next()
-    ctx.status = 200
-    // returned json overwrites the original message
-    // per step 4 here: https://api.slack.com/interactive-messages#lifecycle_of_a_typical_interactive_message_flow
-    ctx.body = {
-      text: '_marked as read_'
-    }
+app.post('/read', async (req, res, next) => {
+  res.status(200).json({
+    text: '_marked as read_'
   })
-)
+})
 
 // send item to slack
-app.use(
-  router.post('/item', async (ctx, next) => {
-    await next()
+app.post('/item', async (req, res, next) => {
+  const body: ItemBody = req.body
+  validateApiKey(req.query.api_key, next)
+  validateInput(body, next)
 
-    const body: ItemBody = ctx.request.body
-    validateApiKey(ctx)
-    validateInput(ctx, body)
+  console.log('going anyway')
+  // console.log(body.url, body.identifier)
+  const handler = pickHanlder(body.url, body.identifier)
+  // console.log(handler)
 
-    // console.log(body.url, body.identifier)
-    const handler = pickHanlder(body.url, body.identifier)
-    // console.log(handler)
+  const response = await handler.postToChannel(
+    body.channel,
+    body.url,
+    slackClient,
+    body.re_parse || false
+  )
+  if (!response.ok) {
+    res.status(500)
+  }
+  res.json(response)
+})
 
-    ctx.body = await handler.postToChannel(
-      body.channel,
-      body.url,
-      slackClient,
-      body.re_parse || false
-    )
-    if (!ctx.body.ok) {
-      ctx.status = 500
-    }
-  })
-)
+// error handler
+const errHandler: express.ErrorRequestHandler = (err, req, res, next) => {
+  res.status(err.status || 500).send({ message: err.message })
+}
+app.use(errHandler)
 
 const listener = app.listen(process.env.PORT || 1234, () => {
   console.log(`app is listening on http://localhost:${listener.address().port}`)
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,22 +1,28 @@
-import { Context } from 'koa'
+import { NextFunction } from 'express'
 import * as _ from 'lodash'
 
-export const validateApiKey = (ctx: Context) => {
+export const validateApiKey = (apiKey: string, next: NextFunction) => {
   if (!process.env.API_KEY) {
-    ctx.throw(500, 'env variables not loaded')
-  } else if (ctx.query.api_key !== process.env.API_KEY) {
-    ctx.throw(403, 'invalid or missing api key')
+    const e = new Error('env variables not loaded')
+    // e.status
+    next(e)
+    // { message: })
+  } else if (apiKey !== process.env.API_KEY) {
+    const e = new Error('invalid or missing api key')
+    // e.status
+    next(e)
+    // { status: 403, message:  })
   }
 }
 
-export const validateInput = (ctx: Context, body: ItemBody) => {
+export const validateInput = (body: ItemBody, next: NextFunction) => {
   if (!(body.channel && body.url)) {
-    ctx.throw(
-      400,
-      `missing params: ${_.filter([
+    next({
+      status: 400,
+      message: `missing params: ${_.filter([
         body.channel ? null : 'channel',
         body.url ? null : 'url'
       ])}`
-    )
+    })
   }
 }
