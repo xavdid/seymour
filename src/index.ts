fcead2d1945a8550f2b701fb4cafc8a4116ad93c
David Brownman <beamneocube@gmail.com>
2018-08-09 10:18:48 -0700

    route setup and main code messy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Express, json, urlencoded, ErrorRequestHandler } from 'express'
 
+import { parse as qsParse } from 'querystring'
+import { parse as urlParse } from 'url'
+
 import * as _ from 'lodash'
 import * as dotenv from 'dotenv'
 import { identifiersByDomain, pickHandler } from '@seymour/handlers'
@@ -8,21 +11,120 @@ import { validateApiKey, validateInput } from './middlewares'
 import { IncomingMessage, ServerResponse } from 'http'
 import * as getRawBody from 'raw-body'
 
-export const reply = async (
+const WebClient = require('@slack/client').WebClient
+const slackClient = new WebClient(process.env.SLACK_API_TOKEN)
+
+/**
+ * helper function for creating a nice response every time
+ */
+const respond = (response: ServerResponse, o: object, statusCode = 200) => {
+  response.statusCode = statusCode
+  response.end(JSON.stringify(o))
+}
+
+type replyFunc = (o: object, statusCode?: number) => void
+interface Route {
+  handler: (reply: replyFunc) => void
+  protected?: boolean
+  description: string
+}
+// const METHODS = 'GET'|| 'POST' || 'PUT' || 'DELETE'
+
+const routes: { [method: string]: { [path: string]: Route } } = {
+  GET: {
+    '/': {
+      description: 'This route, an explanation of everything',
+      handler: reply =>
+        reply({
+          routes: [
+            {
+              description:
+                "mapping of domain to available identifiers. If a domain isn't listed (or you use a listed domain without an identifier), the default handler will be used",
+              route: '/identifiers',
+              method: 'GET'
+            },
+            {
+              description: 'array of Slack channel objects',
+              route: '/channels',
+              method: 'GET'
+            },
+            {
+              description: 'mark an item as read',
+              params: {
+                ts: "message's timestamp",
+                channel: 'slack channel id'
+              },
+              route: '/read',
+              method: 'POST'
+            },
+            {
+              description: 'send an item to a channel',
+              params: {
+                channel: 'slack channel id',
+                identifier:
+                  '[optional] string to mark specific content in a shared domain',
+                url: '[optional] url to a linked post',
+                re_parse:
+                  "[optional, bool] use an extra parser if Slack doesn't unfurl the link"
+              },
+              route: '/item',
+              method: 'POST'
+            }
+          ]
+        })
+    },
+    '/identifiers': {
+      handler: reply => reply(identifiersByDomain),
+      description:
+        "mapping of domain to available identifiers. If a domain isn't listed (or you use a listed domain without an identifier), the default handler will be used"
+    },
+    '/channels': {
+      protected: true,
+      handler: async reply => {
+        const channels = (await slackClient.channels.list()).channels.filter(
+          (i: any) => !i.is_archived
+        )
+        reply(channels.map((channel: any) => _.pick(channel, ['id', 'name'])))
+      },
+      description: 'array of Slack channel objects'
+    }
+  },
+  POST: {
+    '/read': {
+      handler: reply => reply({ text: '_marked as read_' }),
+      description: 'mark an item as read'
+    },
+    '/item': {
+      protected: true,
+      handler: reply => {
+        throw new Error('implement!')
+      },
+      description: 'send an item to a channel'
+    }
+  }
+}
+
+export const serve = async (
   request: IncomingMessage,
   response: ServerResponse
 ) => {
-  // parse the url out
-  if (request.method === 'GET') {
-    response.end(JSON.stringify({ ok: true, query: qs.parse(request.url) }))
-    return
-  } else if (request.method === 'POST') {
-    const body = await getRawBody(request, { encoding: true })
-    response.end(body)
-    console.log('after')
-  } else {
-    response.statusCode = 405
-    response.end(JSON.stringify({ ok: false, status: 405 }))
+  const reply: replyFunc = respond.bind(null, response)
+
+  try {
+    // parse the url out
+    const urlObj = urlParse(request.url!)
+    if (request.method === 'GET') {
+      const query = qsParse(urlObj.query!)
+      reply({ ok: true, query })
+    } else if (request.method === 'POST') {
+      const body = JSON.parse(await getRawBody(request, { encoding: true }))
+      reply(body)
+    } else {
+      reply({ ok: false }, 405)
+    }
+  } catch (e) {
+    response.statusCode = 500
+    response.end({ message: e.message })
   }
 }
 
@@ -34,45 +136,9 @@ export const bootstrap = (app: Express) => {
     dotenv.config()
   }
 
-  const WebClient = require('@slack/client').WebClient
-  const slackClient = new WebClient(process.env.SLACK_API_TOKEN)
-
   // give json instructions
   app.get('/', async (req, res) => {
-    res.json({
-      routes: [
-        {
-          description:
-            "mapping of domain to available identifiers. If a domain isn't listed (or you use a listed domain without an identifier), the default handler will be used",
-          route: '/identifiers',
-          method: 'GET'
-        },
-        {
-          description: 'array of Slack channel objects',
-          route: '/channels',
-          method: 'GET'
-        },
-        {
-          description: 'mark an item as read',
-          params: { ts: "message's timestamp", channel: 'slack channel id' },
-          route: '/read',
-          method: 'POST'
-        },
-        {
-          description: 'send an item to a channel',
-          params: {
-            channel: 'slack channel id',
-            identifier:
-              '[optional] string to mark specific content in a shared domain',
-            url: '[optional] url to a linked post',
-            re_parse:
-              "[optional, bool] use an extra parser if Slack doesn't unfurl the link"
-          },
-          route: '/item',
-          method: 'POST'
-        }
-      ]
-    })
+    res.json()
   })
 
   app.get('/identifiers', (req, res) => {
