01769ab115bbbfa42419a98f28387cad6ae1b283
David Brownman <beamneocube@gmail.com>
2018-11-17 12:24:52 -0500

    maybe moving to express

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,46 @@
-import { createServer } from 'http'
+import * as express from 'express'
 import { listener } from './index'
+import * as helmet from 'helmet'
 
-const hostname = '127.0.0.1'
-const port = 3000
+const app = express()
+app.use(helmet)
 
-const server = createServer(listener).on('listening', () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello!'))
+
+app.listen(port, () => console.log(`Server running on :${port}`))
+
+// const server = createServer(listener).on('listening', () => {
+//   console.log(`Server running at http://${hostname}:${port}/`)
+// })
+
+// export const start = () =>
+//   new Promise((resolve, reject) => {
+//     server.listen(port, hostname, resolve).on('error', reject)
+//   })
+
+// export const stop = () =>
+//   new Promise(resolve => {
+//     server.close(resolve)
+//   })
+
+// export const isRunning = () => server.listening
+
+// if (require.main === module) {
+//   start().catch(err => console.error(err.message))
+// }
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
 })
-
-export const start = () =>
-  new Promise((resolve, reject) => {
-    server.listen(port, hostname, resolve).on('error', reject)
-  })
-
-export const stop = () =>
-  new Promise(resolve => {
-    server.close(resolve)
-  })
-
-export const isRunning = () => server.listening
-
-if (require.main === module) {
-  start().catch(err => console.error(err.message))
-}
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
